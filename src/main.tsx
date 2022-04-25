bbe2abc12472746adbfe8606ab341d40b7a7274a
jp-knj <kenji.tomita1996@gmail.com>
2022-04-25 21:20:14 +0900

    :art: throw error if not have root id

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const isRootElement = document.getElementById('root');
+if (!isRootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(isRootElement);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
