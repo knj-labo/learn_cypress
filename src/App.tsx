5a7b5e0314fe1a11034c3663b6b605eea8a872a8
jp-knj <kenji.tomita1996@gmail.com>
2022-04-26 10:01:06 +0900

    :sparkles: create new todo text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import type { RouteComponentProps } from '@reach/router'
+import React, { useEffect } from 'react'
+import { useRecoilValue } from 'recoil'
 
+import type { AppState } from './dataStructure'
+import { recoilState, LocalStorageKey } from './dataStructure'
+
+import { NewTodoTextInput } from './NewTodoTextInput'
 import { Layout } from './style'
 import { Copyright } from './Copyright'
 
@@ -10,9 +16,20 @@ type Props = {
 }
 
 const App: React.FC<Props & RouteComponentProps> = ({ path }) => {
+    const appState = useRecoilValue<AppState>(recoilState)
+
+    // if appState has changes, save it LocalStorage.
+    useEffect((): void => {
+        window.localStorage.setItem(
+            LocalStorageKey.APP_STATE,
+            JSON.stringify(appState) // convert JavaScript Object to string
+        )
+    }, [appState])
+
     return (
         <Layout>
             <section className="todoapp">
+                <NewTodoTextInput />
             </section>
             <Copyright />
         </Layout>
---
+++ src/dataStructure.ts
@@ -0,0 +1,39 @@
+import type { RecoilState } from 'recoil'
+import { atom } from 'recoil'
+
+export type Todo = {
+    id: string
+    bodyText: string
+    completed: boolean
+}
+
+export type TodoListType = Todo[]
+
+export type AppState = {
+    todoList: TodoListType
+}
+
+export enum LocalStorageKey {
+    APP_STATE = 'APP_STATE',
+}
+
+function LoadAppStateFromLocalStorage(): AppState {
+    const stringifiedJSON: string | null = window.localStorage.getItem(
+        LocalStorageKey.APP_STATE
+    )
+    if (typeof stringifiedJSON === 'string') {
+        const Loaded: AppState = JSON.parse(stringifiedJSON)
+        return Loaded
+    }
+
+    const BlankAppState: AppState = {
+        todoList: [],
+    }
+
+    return BlankAppState
+}
+
+export const recoilState: RecoilState<AppState> = atom({
+    key: 'initialAppState',
+    default: LoadAppStateFromLocalStorage(),
+})
