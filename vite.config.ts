394223a99143e5b517119417e1802f38a19f3d1f
jp-knj <kenji.tomita1996@gmail.com>
2022-04-27 10:16:12 +0900

    :sparkles: install type-alias and setup path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { ViteAliases } from 'vite-aliases'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+      react(),
+      ViteAliases()
+  ],
 })
