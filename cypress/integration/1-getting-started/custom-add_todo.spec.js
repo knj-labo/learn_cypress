50e0d6bec5ae8494d17cd8f9812ffd0d7df44775
jp-knj <kenji.tomita1996@gmail.com>
2022-04-28 10:17:55 +0900

    :green_heart: create test of add todo

--- cypress/integration/1-getting-started/custom-add_todo.spec.js
+++ cypress/integration/1-getting-started/custom-add_todo.spec.js
@@ -1,9 +1,18 @@
 describe('Todoリストを追加', () => {
-    it('can add todo', () => {
-        cy.visit('http://localhost:3000/')
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
 
-        // can type text and submit
+    const TYPE_SOMETHING = 'can be typing'
+    it(`${TYPE_SOMETHING} を input に入力し、enter を押したときは テキストは ${TYPE_SOMETHING} のTodoアイテムが存在する`, () => {
         cy.get('[data-cy=new-todo-input-text]')
-            .type('can be typing')
+            .type(TYPE_SOMETHING)
+            .type('{enter}')
+            .should('not.have.value')
+
+        // it is added a submited todo
+        cy.get('[data-cy=todo-item]')
+            .should('exist')
+            .should('contain', TYPE_SOMETHING)
     })
 });
