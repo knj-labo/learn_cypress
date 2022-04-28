2fd16bacac1e15f5d6c2ca069a077f5d5815f586
jp-knj <kenji.tomita1996@gmail.com>
2022-04-28 10:35:07 +0900

    green_heart: can not add space char only text to todo

--- cypress/integration/1-getting-started/custom-add_todo.spec.js
+++ cypress/integration/1-getting-started/custom-add_todo.spec.js
@@ -14,5 +14,43 @@ describe('Todoリストを追加', () => {
         cy.get('[data-cy=todo-item]')
             .should('exist')
             .should('contain', TYPE_SOMETHING)
+    });
+
+    const TYPE_FIRST_PATTERN = 'type first pattern'
+    const TYPE_SECOND_PATTERN = 'type second pattern'
+    const TYPE_THIRD_PATTERN = 'type third pattern'
+    it('Todoリストは複数のアイテムを作成', () => {
+        cy.get('[data-cy=new-todo-input-text]')
+            .type(TYPE_FIRST_PATTERN)
+            .type('{enter}')
+            .should('not.have.value')
+
+        // it is added a submited todo
+        cy.get('[data-cy=todo-item]')
+            .should('exist')
+            .should('contain', TYPE_FIRST_PATTERN)
+
+        // can add 2 more todos
+        cy.get('[data-cy=new-todo-input-text]')
+            .type(TYPE_SECOND_PATTERN)
+            .should('have.value', TYPE_SECOND_PATTERN)
+            .type('{enter}')
+            .should('not.have.value')
+        cy.get('[data-cy=todo-item]').should('exist').should('contain', TYPE_SECOND_PATTERN)
+
+        cy.get('[data-cy=new-todo-input-text]')
+            .type(TYPE_THIRD_PATTERN)
+            .should('have.value', TYPE_THIRD_PATTERN)
+            .type('{enter}')
+            .should('not.have.value')
+        cy.get('[data-cy=todo-item]').should('exist').should('contain', TYPE_THIRD_PATTERN)
+    })
+
+    it('空白スペースのみの場合はTodoアイテムを作成できない', () => {
+        // submit space only input that submit should be disallowed
+        cy.get('[data-cy=new-todo-input-text]').type('       ').type('{enter}')
+
+        // there is no added todo item
+        cy.get('[data-cy=todo-item]').should('not.exist')
     })
 });
