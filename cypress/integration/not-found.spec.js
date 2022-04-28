6d72c7c9a868bfd8192650cb1ad21e21d5d03a63
jp-knj <kenji.tomita1996@gmail.com>
2022-04-28 11:54:17 +0900

    :sparkles: create not found page

---
+++ cypress/integration/not-found.spec.js
@@ -0,0 +1,10 @@
+describe('', () => {
+    it('should show not found page', () => {
+        // no exist uri
+        cy.visit('http://localhost:3000/sdg3y3457jtdyjgbvn')
+
+        cy.get('[data-cy=not-found-page]')
+            .should('exist')
+            .should('contain', 'Page Not Found')
+    })
+});
