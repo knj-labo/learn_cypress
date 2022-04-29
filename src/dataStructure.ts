aa7fc73f05c31bb4b8c3e20e3e509087f497bb28
jp-knj <kenji.tomita1996@gmail.com>
2022-04-29 23:27:03 +0900

    :shirt: execute eslint for utility files

--- src/dataStructure.ts
+++ src/dataStructure.ts
@@ -1,39 +1,39 @@
-import type { RecoilState } from 'recoil'
-import { atom } from 'recoil'
+import type { RecoilState } from "recoil";
+import { atom } from "recoil";
 
 export type Todo = {
-    id: string
-    bodyText: string
-    completed: boolean
-}
+  id: string;
+  bodyText: string;
+  completed: boolean;
+};
 
-export type TodoListType = Todo[]
+export type TodoListType = Todo[];
 
 export type AppState = {
-    todoList: TodoListType
-}
+  todoList: TodoListType;
+};
 
 export enum LocalStorageKey {
-    APP_STATE = 'APP_STATE',
+  APP_STATE = "APP_STATE",
 }
 
 const LoadAppStateFromLocalStorage = (): AppState => {
-    const stringifiedJSON: string | null = window.localStorage.getItem(
-        LocalStorageKey.APP_STATE
-    )
-    if (typeof stringifiedJSON === 'string') {
-        const Loaded: AppState = JSON.parse(stringifiedJSON)
-        return Loaded
-    }
+  const stringifiedJSON: string | null = window.localStorage.getItem(
+    LocalStorageKey.APP_STATE
+  );
+  if (typeof stringifiedJSON === "string") {
+    const Loaded: AppState = JSON.parse(stringifiedJSON);
+    return Loaded;
+  }
 
-    const BlankAppState: AppState = {
-        todoList: [],
-    }
+  const BlankAppState: AppState = {
+    todoList: [],
+  };
 
-    return BlankAppState
-}
+  return BlankAppState;
+};
 
 export const recoilState: RecoilState<AppState> = atom({
-    key: 'initialAppState',
-    default: LoadAppStateFromLocalStorage(),
-})
+  key: "initialAppState",
+  default: LoadAppStateFromLocalStorage(),
+});
--- src/style.ts
+++ src/style.ts
@@ -2,7 +2,7 @@
 /* Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com) */
 /* This source code was getting from https://github.com/tastejs/todomvc-app-css/blob/03e753aa21bd555cbdc2aa09185ecb9905d1bf16/index.css */
 
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
 
 export const Layout = styled.div`
   .todoapp {
@@ -56,10 +56,10 @@ export const Layout = styled.div`
   .info a:hover {
     text-decoration: underline;
   }
-`
+`;
 
 export const base = {
-    textInput: css`
+  textInput: css`
     position: relative;
     margin: 0;
     width: 100%;
@@ -75,4 +75,4 @@ export const base = {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   `,
-}
+};
