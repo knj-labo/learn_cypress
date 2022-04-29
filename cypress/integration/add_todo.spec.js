cb4db071df0c6e5392630c7078fdfaa276d66479
jp-knj <kenji.tomita1996@gmail.com>
2022-04-29 23:24:16 +0900

    :shirt: execute eslint for cypress

--- cypress/integration/add_todo.spec.js
+++ cypress/integration/add_todo.spec.js
@@ -1,56 +1,60 @@
-describe('Todoリストを追加', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000')
-    })
+describe("Todoリストを追加", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
 
-    const TYPE_SOMETHING = 'can be typing'
-    it(`${TYPE_SOMETHING} を input に入力し、enter を押したときは テキストは ${TYPE_SOMETHING} のTodoアイテムが存在する`, () => {
-        cy.get('[data-cy=new-todo-input-text]')
-            .type(TYPE_SOMETHING)
-            .type('{enter}')
-            .should('not.have.value')
+  const TYPE_SOMETHING = "can be typing";
+  it(`${TYPE_SOMETHING} を input に入力し、enter を押したときは テキストは ${TYPE_SOMETHING} のTodoアイテムが存在する`, () => {
+    cy.get("[data-cy=new-todo-input-text]")
+      .type(TYPE_SOMETHING)
+      .type("{enter}")
+      .should("not.have.value");
 
-        // it is added a submited todo
-        cy.get('[data-cy=todo-item]')
-            .should('exist')
-            .should('contain', TYPE_SOMETHING)
-    });
+    // it is added a submited todo
+    cy.get("[data-cy=todo-item]")
+      .should("exist")
+      .should("contain", TYPE_SOMETHING);
+  });
 
-    const TYPE_FIRST_PATTERN = 'type first pattern'
-    const TYPE_SECOND_PATTERN = 'type second pattern'
-    const TYPE_THIRD_PATTERN = 'type third pattern'
-    it('Todoリストは複数のアイテムを作成', () => {
-        cy.get('[data-cy=new-todo-input-text]')
-            .type(TYPE_FIRST_PATTERN)
-            .type('{enter}')
-            .should('not.have.value')
+  const TYPE_FIRST_PATTERN = "type first pattern";
+  const TYPE_SECOND_PATTERN = "type second pattern";
+  const TYPE_THIRD_PATTERN = "type third pattern";
+  it("Todoリストは複数のアイテムを作成", () => {
+    cy.get("[data-cy=new-todo-input-text]")
+      .type(TYPE_FIRST_PATTERN)
+      .type("{enter}")
+      .should("not.have.value");
 
-        // it is added a submited todo
-        cy.get('[data-cy=todo-item]')
-            .should('exist')
-            .should('contain', TYPE_FIRST_PATTERN)
+    // it is added a submited todo
+    cy.get("[data-cy=todo-item]")
+      .should("exist")
+      .should("contain", TYPE_FIRST_PATTERN);
 
-        // can add 2 more todos
-        cy.get('[data-cy=new-todo-input-text]')
-            .type(TYPE_SECOND_PATTERN)
-            .should('have.value', TYPE_SECOND_PATTERN)
-            .type('{enter}')
-            .should('not.have.value')
-        cy.get('[data-cy=todo-item]').should('exist').should('contain', TYPE_SECOND_PATTERN)
+    // can add 2 more todos
+    cy.get("[data-cy=new-todo-input-text]")
+      .type(TYPE_SECOND_PATTERN)
+      .should("have.value", TYPE_SECOND_PATTERN)
+      .type("{enter}")
+      .should("not.have.value");
+    cy.get("[data-cy=todo-item]")
+      .should("exist")
+      .should("contain", TYPE_SECOND_PATTERN);
 
-        cy.get('[data-cy=new-todo-input-text]')
-            .type(TYPE_THIRD_PATTERN)
-            .should('have.value', TYPE_THIRD_PATTERN)
-            .type('{enter}')
-            .should('not.have.value')
-        cy.get('[data-cy=todo-item]').should('exist').should('contain', TYPE_THIRD_PATTERN)
-    })
+    cy.get("[data-cy=new-todo-input-text]")
+      .type(TYPE_THIRD_PATTERN)
+      .should("have.value", TYPE_THIRD_PATTERN)
+      .type("{enter}")
+      .should("not.have.value");
+    cy.get("[data-cy=todo-item]")
+      .should("exist")
+      .should("contain", TYPE_THIRD_PATTERN);
+  });
 
-    it('空白スペースのみの場合はTodoアイテムを作成できない', () => {
-        // submit space only input that submit should be disallowed
-        cy.get('[data-cy=new-todo-input-text]').type('       ').type('{enter}')
+  it("空白スペースのみの場合はTodoアイテムを作成できない", () => {
+    // submit space only input that submit should be disallowed
+    cy.get("[data-cy=new-todo-input-text]").type("       ").type("{enter}");
 
-        // there is no added todo item
-        cy.get('[data-cy=todo-item]').should('not.exist')
-    })
+    // there is no added todo item
+    cy.get("[data-cy=todo-item]").should("not.exist");
+  });
 });
