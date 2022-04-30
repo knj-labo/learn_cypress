5aa364c3743cdeef7b849cf787288375313b21c1
jp-knj <kenji.tomita1996@gmail.com>
2022-04-30 12:34:20 +0900

    :sparkles: add test for filtering completed todo items when completed was selected

--- cypress/integration/filter_todo.spec.js
+++ cypress/integration/filter_todo.spec.js
@@ -18,4 +18,28 @@ context("Filter", () => {
     cy.location().should((loc) => expect(loc.pathname).to.eq("/"));
     cy.get("[data-cy=todo-item]").should("have.length", 3);
   });
+
+  it('"Active"が選択されたとき、urlは /active で未完了の項目を表示されている', () => {
+    cy.get("[data-cy=active-filter]").click();
+    cy.location().should((loc) => expect(loc.pathname).to.eq("/active"));
+    cy.get("[data-cy=todo-item]").should("have.length", 3);
+    // done the todo
+    cy.get("[data-cy=todo-item]:first-of-type")
+      .find("[data-cy=todo-item-complete-check]")
+      .check();
+    cy.get("[data-cy=todo-item]").should("have.length", 2);
+  });
+
+  it('"Completed"が選択されたとき、urlは /completed で完了された項目を表示されている', () => {
+    cy.get("[data-cy=completed-filter]").click();
+    cy.location().should((loc) => expect(loc.pathname).to.eq("/completed"));
+    cy.get("[data-cy=todo-item]").should("not.exist");
+    // done the todo
+    cy.get("[data-cy=all-filter]").click();
+    cy.get("[data-cy=todo-item]:first-of-type")
+      .find("[data-cy=todo-item-complete-check]")
+      .check();
+    cy.get("[data-cy=completed-filter]").click();
+    cy.get("[data-cy=todo-item]").should("have.length", 1);
+  });
 });
